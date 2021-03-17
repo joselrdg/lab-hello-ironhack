c1858ecbf1b5a62bd2648a8b318e872e2cd66e25
joselrdg <josesietepicos@gmail.com>
2021-03-17 16:26:43 +0100

    done

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/css/Header.css';
+
+const Header = () => {
+    return (
+        <header className='Header'>
+            <img src='/images/ironhack-logo.svg' alt='Logo Ironhack'/>
+            <img src='/images/menu-top.svg' alt='menu'/>
+        </header>)
+}
+
+export default Header
---
+++ src/components/ListItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../assets/css/ListItem.css';
+
+
+const ListItem = ({ title, description, img }) => {
+    return (
+        <div className='ListItem'>
+            <img src={img.src} alt={img.alt} />
+            <h4>{title}</h4>
+            <p>{description}</p>
+        </div>
+    );
+};
+
+export default ListItem;
